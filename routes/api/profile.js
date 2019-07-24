b67c51bef36dc4ca915f431ae26e72618e4b44d6
ischdry <ichowdhury4@fordham.edu>
2019-07-24 14:51:19 -0400

    get profile by id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -113,5 +113,52 @@ router.post('/', [auth, [
         }
     );
 
+//@route GET request api/profile/me
+//desc   Get all profiles
+//@acess Public
+
+router.get('/', async (req,res) => {
+
+    try {
+        const profiles = await Profile.find ().populate('user', ['name', 'avatar']); 
+        res.json(profiles);
+        
+    } catch (error) {
+        console.log (error.message);
+        res.status(500);
+    }
+
+});
+
+//@route GET request api/profile/user/:user_id
+//desc   Get profile by user ID
+//@acess Public
+
+router.get('/user/:user_id', async (req, res) => {
+
+    try {
+        const profile = await Profile.findOne({
+            user : req.params.user_id //on link
+        }).populate('user', ['name', 'avatar']);
+
+        if (!profile) return res.status(400).json({
+                msg : "There is no profile for this user"
+            });
+        
+        res.json(profile);
+
+    } catch (error) {
+        console.log(error.message);
+        if (err.kind == 'ObjectId') { return res.status(400).json({
+            msg: "There is no profile for this user"
+        
+        });
+     }
+             res.status(500);
+
+    }
+
+});
+
 
 module.exports = router;
