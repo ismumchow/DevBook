7ddd50405e051e4319ff69fbfad613c6d3caab81
ischdry <ichowdhury4@fordham.edu>
2019-07-29 21:47:51 -0400

    delete experience request

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -241,6 +241,33 @@ router.put ('/experience',[auth, [
         res.status(500).send('Server Error')
     }
     
-})
+}); 
+
+
+//@route DEL request api/profile/experience/:exp_id
+//desc  delete profile experience
+//@acess Private
+ router.delete ('/experience/:exp_id', auth, async (req,res)=> {
+    try {
+
+        const profile = await Profile.findOne({
+            user: req.user.id
+        });
+
+        const removeIndex = profile.experience
+        .map(item =>item.id)
+        .indexOf(req.params.exp_id);
+        profile.experience.splice(removeIndex,1); 
+
+        await profile.save();
+
+        res.json(profile);
+        
+    } catch (error) {
+        console.log(error)
+        res.status(400).send('server error');
+    }
+
+ })
 
 module.exports = router;
