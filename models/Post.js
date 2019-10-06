237cbdfe698f06e16f8460e98b8867db974912a5
ischdry <ichowdhury4@fordham.edu>
2019-10-06 01:10:01 -0400

    post route created

--- models/Post.js
+++ models/Post.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose'); 
 const Schema = mongoose.Schema; 
 
-const PostSchema = mongoose.Schema; 
-
 const PostSchema = new Schema ({ 
     user: {
         type: Schema.Types.ObjectId, 
